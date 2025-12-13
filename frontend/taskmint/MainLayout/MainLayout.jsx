833e0197da2297e8d34b08f827a4ae042a840860
Palden D. Titung <paldendorjetitung@gmail.com>
2025-12-13 12:28:50 +0545

    changed

--- frontend/taskmint/MainLayout/MainLayout.jsx
+++ frontend/taskmint/MainLayout/MainLayout.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBar from "../Components/SideBar";
 const MainLayout = ({ children }) => {
   return (
-    <main className="  bg-[rgb(25,25,25)] text-white flex  ">
-      <aside className="h-screen w-[320px]  fixed border-r border-white">
+    <main className="  bg-[rgb(25,25,25)] text-white flex flex-col md:flex-row  ">
+      <aside className="h-screen  w-[320px]  fixed border-r border-white">
         <SideBar />
       </aside>
       <section className="h-screen ml-80 w-full ">{children}</section>
